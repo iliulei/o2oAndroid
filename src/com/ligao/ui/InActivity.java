83ba08635f8ae6b8f29b89cdacecb1790a2d7696
iliulei <leol66@163.com>
2016-10-31 16:42:41 +0800

    入库模块开发

---
+++ src/com/ligao/ui/InActivity.java
@@ -0,0 +1,151 @@
+package com.ligao.ui;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ligao.R;
+import com.ligao.app.MyApplication;
+import com.ligao.bean.Configer;
+import com.ligao.bean.Constants;
+import com.ligao.entity.Goods;
+import com.ligao.entity.User;
+import com.ligao.shoppingcart.CheckOutActivity;
+import com.ligao.shoppingcart.ShopBean;
+import com.ligao.shoppingcart.ShoppingCanst;
+import com.ligao.ui.base.BaseActivity;
+import com.ligao.utils.KsoapUtil;
+import com.ligao.utils.ToastUtil;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+public class InActivity extends BaseActivity  implements OnClickListener{
+	
+	private EditText  mNumberEdt;
+	
+	private TextView goBack;
+	private Button sumbitBt;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_in);
+		
+		
+		sumbitBt = (Button)findViewById(R.id.bt_submit);
+		goBack = (TextView) findViewById(R.id.go_back);
+		mNumberEdt = (EditText) findViewById(R.id.edt_in_mNumber);
+		
+		sumbitBt.setOnClickListener(this);
+		goBack.setOnClickListener(this);
+	}
+
+	@Override
+	protected void findViewById() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initView() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void submit(){
+		new Thread(getJson).start();
+		ToastUtil.showToast(this, "确定");
+	}
+	
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		case R.id.bt_submit:
+			submit();
+			break;
+		case R.id.go_back:
+			finish();
+			break;
+		default:
+			break;
+		}
+	}
+	Handler handler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			if (msg.what == 0x00) {
+				Log.v("PostGetJson", "" + result);
+				
+				if(null != result){
+					try {
+						JSONObject json= new JSONObject(result);  
+						String re = (String)json.get("Message");
+						Object JsonStr = (Object)json.get("JsonStr");
+						if(!JsonStr.equals(null)){
+							json=  new JSONObject(JsonStr.toString());
+							MyApplication application= (MyApplication)getApplication();
+							User user = new User();
+							user.setName((String)json.get("Name"));
+							user.setLoginName((String)json.get("LoginName"));
+							user.setDeptCode((String)json.get("DeptCode"));
+							user.setDeptName((String)json.get("DeptName"));
+							application.setUserInfo(user);
+						}
+						//execute(re);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}  
+				}else{
+					//execute(Constants.SERVERERROR);
+				}
+				
+			} else if (msg.what == 0x01) {
+				ToastUtil.showToast(InActivity.this, "服务器链接错误!");
+				/*Toast.makeText(getApplicationContext(), "获取失败",
+						Toast.LENGTH_LONG).show();*/
+			}
+		}
+	};
+	private String result;
+	private String myurl,soap_action;
+	private Runnable getJson = new Runnable() {
+
+		public void run() {
+			try {
+				SoapObject rpc = new SoapObject(Configer.NAMESPACE, Configer.WcfMethod_TotalWareHouseEnter);
+				rpc.addProperty("wwaybillInfoJson", mNumberEdt.getText().toString().trim());
+		    	myurl = Configer.getWcfUrl(getApplicationContext());
+		    	soap_action = Configer.SOAPACTION_FRONT+Configer.WcfMethod_TotalWareHouseEnter;
+		    	result = KsoapUtil.GetJsonWcf(rpc, myurl, soap_action);
+				//result = GetJson(myurl, params);
+				if("error".equals(result))
+					handler.sendEmptyMessage(0x01);
+				else
+					handler.sendEmptyMessage(0x00);
+				
+			} catch (Exception e) {
+				handler.sendEmptyMessage(0x01);
+			}
+		}
+	};
+
+
+}
---
+++ src/com/ligao/utils/KsoapUtil.java
@@ -0,0 +1,50 @@
+package com.ligao.utils;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+/**
+ * KsoapUtil 工具类
+ * @author Administrator
+ *
+ */
+public class KsoapUtil {
+	
+	    /**
+	     * 发送消息
+	     * @param rpc
+	     * @param myUrl
+	     * @param soap_action
+	     * @return 服务器返回数据
+	     */
+	    public static   String GetJsonWcf(SoapObject rpc, String myUrl,String soap_action) {
+		String result = "ok";
+    	// 生成调用WebService方法的SOAP请求信息,并指定SOAP的版本  
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER10);  
+        envelope.bodyOut = rpc;  
+        // 设置是否调用的是dotNet开发的WebService  
+        envelope.dotNet = true;
+        HttpTransportSE transport = new HttpTransportSE(myUrl,5*1000);  
+        try {  
+            // 调用WebService  
+            transport.call(soap_action, envelope);  
+        } catch (Exception e) {  
+        	//代表连接失败
+            e.printStackTrace();  
+            result = "error";
+            System.out.println("连接失败!");
+        }  
+        // 获取返回的数据  
+        SoapObject object = (SoapObject) envelope.bodyIn; 
+        if(null==object){
+        	//return;
+        }else
+        // 获取返回的结果  
+        {
+        	  result = object.getProperty(0).toString();
+        }
+		return result;
+	}
+}
